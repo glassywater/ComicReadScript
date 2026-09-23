22f6abd76c2445e3d97172b45e64763deb142279
hymbz <hymbz3516@gmail.com>
2026-09-23 21:47:32 +0800

    fix: :bug: 修复绅士漫画改版导致的失效
    #374 #375 340303#comment-668448

--- src/site/wnacg.tsx
+++ src/site/wnacg.tsx
@@ -11,14 +11,15 @@ import { querySelector, t } from 'helper';
 
   // 旧版：https://www.wnacg.com/photos-slist-aid-376223.html
   // 新版：https://www.wnacg.com/photos-slide-aid-376223.html
-  // 条漫（？）：https://www.wnacg.com/photos-list-aid-376223.html
+  // 条漫（条漫会默认使用这个 url，但实际和旧版一模一样，也可以加载其他漫画）：
+  // https://www.wnacg.com/photos-list-aid-376223.html
   // 阅读器有多种类型，网址始终是 photos-<类型>-aid-<id> 的格式
   // <类型> 仅影响阅读器，不影响资源的加载
 
   const match = /\/photos-(?<type>slist|slide|list)-aid-(?<id>\d+)/u.exec(
     location.pathname,
   )?.groups;
-  if (!match?.type || !match?.id || match?.type === 'index') return;
+  if (!match?.type || !match?.id) return;
 
   const getImgList: SetupOptions['getImgList'] | undefined =
     unsafeWindow.imglist
@@ -32,11 +33,15 @@ import { querySelector, t } from 'helper';
           const res = await request<string>(
             `/photos-item-aid-${match.id}.html`,
           );
-          const pageUrl = /"page_url":(?<pageUrl>\[.+\]),/u.exec(
-            res.responseText,
-          )?.groups.pageUrl;
-          if (!pageUrl) throw new Error(t('site.changed_load_failed'));
-          return eval(pageUrl) as string[]; // oxlint-disable-line no-eval
+          const data = /\.initData\((?<data>\{.+\})\);/u.exec(res.responseText)
+            ?.groups.data;
+          if (!data) throw new Error(t('site.changed_load_failed'));
+          // 数据里有尾逗号，删掉后才能用 JSON.parse 解析
+          return (
+            JSON.parse(data.replaceAll(/,\s*(?=[\]}])/gu, '')) as {
+              page_url: string[];
+            }
+          ).page_url.map((url) => url.replace(/^http:\/\//u, 'https://'));
         };
 
   setup({ name: 'wnacg', getImgList });
